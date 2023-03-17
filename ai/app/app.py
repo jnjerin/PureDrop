44c1fe78b903819a118f446e9d5726a2c4e169b1
davidnene <davienesh4@gmail.com>
2023-03-17 17:36:27 +0300

    Setup templates

--- ai/app/app.py
+++ ai/app/app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 cors = CORS(app, resources={
     r"/*":{
         "origins":"*"
     }
 })
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
