55f34b967ce146e0bc090ec6760d50794eb380d3
jnjerin <njerijoan24@gmail.com>
2023-03-18 23:19:14 +0300

    Entrypoint update

--- puredrop-frontend/src/App.js
+++ puredrop-frontend/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { ColorModeContext, useMode } from "./theme";
+
+import Sidenav from "./components/Sidenav";
+import Topbar from "./components/Topbar";
+
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { useState } from "react";
+import Dashboard from "./pages/dashboard/Dashboard";
+import BarChart from "./components/BarChart";
+import LineChart from "./components/LineChart";
 
 function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidenav isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/bar" element={<BarChart />} />
+              <Route path="/line" element={<LineChart />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
