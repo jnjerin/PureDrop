22a4abfa69bb09450ccdb2b31201bc64eee52c64
jnjerin <njerijoan24@gmail.com>
2023-03-20 06:13:19 +0300

    added routing

--- puredrop-backend/config/routes.rb
+++ puredrop-backend/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post '/login' => 'auth#login'
+  get '/me', to: 'users#show_me'
+  put '/profile/update', to: 'profiles#update_profile'
+  post '/signup', to: 'users#create'
 end
